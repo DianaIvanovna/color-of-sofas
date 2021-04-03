92c71c5b9f40da173c8d5a96366714ce4403da87
DianaIvanovna <GDI.1999@yandex.ru>
2021-04-03 21:39:27 +0300

    feat: auth in social networks

--- src/component/authSocialNetwork/AuthSocialNetwork.js
+++ src/component/authSocialNetwork/AuthSocialNetwork.js
@@ -1,22 +1,58 @@
 import React from 'react';
-import axios from'axios';
 import classes from './AuthSocialNetwork.module.scss';
 import vk from "../../img/vk.svg";
 import fb from "../../img/fb.svg";
 
 const AuthSocialNetwork = props => {
+  // функция загрузки фото с вк
   const authVk = () => {
-    // axios.get('https://oauth.vk.com/authorize?client_id=7804077&display=page&redirect_uri=http://garbuzdiana.tmweb.ru/settings&scope=friends&response_type=token&v=5.52')
-    // .then(function (response) {
-    //   console.log(response);
-    // })
-    // .catch(function (error) {
-    //   // handle error
-    //   console.log(error);
-    // })
-    // .then(function () {
-    //   // always executed
-    // });
+    // eslint-disable-next-line no-undef
+    VK.init({
+      apiId: 7804077
+    });
+    // eslint-disable-next-line no-undef
+    VK.Auth.login((response)=>{
+      if (response.session) { /* Пользователь успешно авторизовался */
+        // eslint-disable-next-line no-undef
+        VK.Api.call('users.get', {
+          user_ids: response.session.user.id,
+          fields: 'photo_max',
+          v:"5.73"},
+          function(r) {
+            console.log(r.response[0].photo_max); // фото
+          });
+      } else {
+      }
+      // eslint-disable-next-line no-undef
+    }, VK.access.PHOTOS)
+  }
+
+  const authFB = ()=>{
+  // eslint-disable-next-line no-undef
+  FB.init({
+    appId            : '202220517908280',
+    autoLogAppEvents : true,
+    xfbml            : true,
+    version          : 'v10.0'
+  });
+
+  // eslint-disable-next-line no-undef
+  FB.login(function(response) {
+    if (response.status === 'connected') { //Logged into your webpage and Facebook.
+      // eslint-disable-next-line no-undef
+      FB.api(
+        `/me/picture?redirect=false`,
+        'GET',
+        {"type":"normal"},
+        function(response) {
+            console.log(response.data.url) // фото из фейсбука
+        }
+      );
+    } else {
+      // The person is not logged into your webpage or we are unable to tell.
+    }
+  }, {scope: 'user_photos'});
+
   }
 
   return (
@@ -24,7 +60,7 @@ const AuthSocialNetwork = props => {
       <div className={classes.icon} onClick={authVk}>
         <img width={23} height={12} src={vk} alt="icon vk"></img>
       </div>
-      <div className={classes.icon}>
+      <div className={classes.icon} onClick={authFB}>
         <img width={12.5} height={23} src={fb} alt="icon facebook"></img>
       </div>
     </div>
